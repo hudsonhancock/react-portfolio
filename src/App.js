b3fd4b3a1ff0da764121321b9c08a30aab64255d
Hudson Hancock <hudsonhancock@gmail.com>
2021-11-07 07:58:58 -0500

    Adds .gitignore

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <h2>React Portfolio</h2>;
+  return <h2>React Portfolio, Hello!</h2>;
 }
 
 export default App;
